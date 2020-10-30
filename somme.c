781aff7cc8988d1b26c746f61d57db0aef23e88d
mohamedazizbenchedly <mohamedaziz.benchedly@esprit.tn>
2020-10-30 22:56:46 +0100

    mise a jour de fichier somme.c

---
+++ somme.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+int a,b,s;
+printf(" donner a \n";
+scanf("%d",&a);
+printf("donner b \n");
+scanf("%d",&b); 
+s=a+b; 
+printf("votre somme est %d \n",s);
+return 0; 
+}
